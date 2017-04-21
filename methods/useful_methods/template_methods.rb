96e44a266bb604ff33561cdc779e9fca6b2ab41a
krain <krain@krainarnold.com>
2017-04-21 16:19:40 +0200

    add error message to template_methods.rb

---
+++ methods/useful_methods/template_methods.rb
@@ -0,0 +1,8 @@
+# This is a custom error message that logs an error to automate.log
+# and changes the ae_result to error
+def error(msg)
+  $evm.log(:error, msg)
+  $evm.root['ae_result'] = 'error'
+  $evm.root['ae_reason'] = msg.to_s
+  exit MIQ_OK
+end
