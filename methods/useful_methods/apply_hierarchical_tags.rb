4f18c817eb83b6fd1aad7b15d9d9d60f5b0ed439
krain <krain@krainarnold.com>
2019-02-27 11:01:05 +0100

    add tag_category and tag_failsafe variables

--- methods/useful_methods/apply_hierarchical_tags.rb
+++ methods/useful_methods/apply_hierarchical_tags.rb
@@ -1,5 +1,5 @@
 #This Method is intended to be used as the Custom Automation in a Control
-#Policy The Policy event should be VM Create Complete, and the Policy
+#Policy. The Policy event should be VM Create Complete, and the Policy
 #Profile should be applied at the Infrastructure Provider level.
 
 @DEBUG = false
@@ -8,20 +8,21 @@ def log(msg, level = :info)
   $evm.log(level, "#{msg}")
 end
 
+tag_category = "environment"
+tag_failsafe = "orphan"
+
 begin
 #Create the VM object
 vm = $evm.root['vm']
 log("The VM name is #{vm.name}") if @DEBUG  
 
-#Create the Orphan Environment Tag if it doesn't already exist
-$evm.execute('tag_create', 'environment', :name=> 'orphan', :description => 'orphan') unless $evm.execute('tag_exists?', 'environment', 'orphan')
+#Create the failsafe tag if it doesn't already exist
+$evm.execute('tag_create', tag_category, :name=> tag_failsafe, :description => tag_failsafe) unless $evm.execute('tag_exists?', tag_category, tag_failsafe)
 
-#Retrieve the environment tag from the parent cluster  
-environment_tag = vm.ems_cluster.tags(:environment).first
-log("The Cluster Environment is #{environment_tag}") if @DEBUG
+#Retrieve the relevant tag from the parent cluster  
+cluster_tag = vm.ems_cluster.tags(tag_category.to_sym).first
+log("The Cluster Tag is #{cluster_tag}") if @DEBUG
 
-#Assign the Cluster Environment to the VM if the tag exists, otherwise assign the Orphan tag
-environment_tag ? vm.tag_assign("environment/#{environment_tag}") : vm.tag_assign("environment/orphan")
-
+#Assign the cluster tag to the VM if the tag exists, otherwise assign the failsafe tag
+cluster_tag ? vm.tag_assign("#{tag_category}/#{cluster_tag}") : vm.tag_assign("#{tag_category}/#{tag_failsafe}")
 end
-
